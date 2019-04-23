735f2dc3d63457bc09e909484520d63708495b43
bucai <1450941858@qq.com>
2019-04-23 21:14:45 +0800

    更换图片处理库

--- dll/login.js
+++ dll/login.js
@@ -3,7 +3,7 @@ const { jvtc_post, jvtc_get } = require('../utlis/jvtc_request');
 const { parsCookies } = require('../utlis/jvtc_pars');
 const { login, img_code } = require('../apis/api');
 const ocrClient = require('../bin/OcrClient');
-const images = require("images");
+const gm = require('gm')
 const superagent = require('superagent');
 
 async function jvtc_fun({ loginName, loginPwd }) {
@@ -44,58 +44,58 @@ async function jvtc_fun({ loginName, loginPwd }) {
     const res = await superagent.get(img_code)
       .set('Cookie', this.o.cookies);
 
-    const image = images(res.body).encode("jpg", { operation: 100 });
+    gm(res.body).toBuffer('jpg', (err, buffer) => {
+      if(err) throw err;
 
-    ocrClient.generalBasic(image.toString("base64")).then((result) => {
+      ocrClient.generalBasic(buffer.toString("base64")).then((result) => {
 
+        const words_result = result['words_result'];
+        const code = words_result && words_result.length && words_result[0].words;
 
-      const words_result = result['words_result'];
-      const code = words_result && words_result.length && words_result[0].words;
-
-      if (!code || code.length != 5 || String(parseInt(code)).length != 5) {
-        throw new Error("识别错误");
-      }
-
-      // 新接口
-      const args = {
-        ...this.o.args,
-        'UserName': loginName,
-        'UserPass': loginPwd,
-        "CheckCode": parseInt(code)
-      }
-
-      args['Btn_OK.x'] = ~~(Math.random() * 30 + 10);
-      args['Btn_OK.y'] = ~~(Math.random() * 30 + 10);
-
-      // console.log(o.args);
-      jvtc_post(login, { cookies: this.o.cookies, args }, (err, res) => {
-        try {
-          const $ = cheerio.load(res.text);
-          const html = new String($("script").html())
-          if (html) {
-            const rex = /alert\('(.*?)'\);/;
-            const ms = html.match(rex);
-            if (ms && ms.length >= 2) {
-              throw ms[1];
-            }
-          }
-          this.o.cookies += parsCookies(res.headers);
-          // 登陆成功标志
-          // this.isLogin = true;
-          resolve([null, 0]);
-
-        } catch (error) {
-          console.log(error);
-          reject(error);
+        if (!code || code.length != 5 || String(parseInt(code)).length != 5) {
+          throw new Error("识别错误");
         }
+
+        // 新接口
+        const args = {
+          ...this.o.args,
+          'UserName': loginName,
+          'UserPass': loginPwd,
+          "CheckCode": parseInt(code)
+        }
+
+        args['Btn_OK.x'] = ~~(Math.random() * 30 + 10);
+        args['Btn_OK.y'] = ~~(Math.random() * 30 + 10);
+
+        // console.log(o.args);
+        jvtc_post(login, { cookies: this.o.cookies, args }, (err, res) => {
+          try {
+            const $ = cheerio.load(res.text);
+            const html = new String($("script").html())
+            if (html) {
+              const rex = /alert\('(.*?)'\);/;
+              const ms = html.match(rex);
+              if (ms && ms.length >= 2) {
+                throw ms[1];
+              }
+            }
+            this.o.cookies += parsCookies(res.headers);
+            // 登陆成功标志
+            // this.isLogin = true;
+            resolve([null, 0]);
+
+          } catch (error) {
+            console.log(error);
+            reject(error);
+          }
+        });
+
+      }).catch(function (err) {
+        // 如果发生网络错误
+        console.log(err);
+        reject(error);
       });
-
-    }).catch(function (err) {
-      // 如果发生网络错误
-      console.log(err);
-      reject(error);
     });
-
 
   }).catch((error) => {
     return [error, null];
--- tests/1.js
+++ tests/1.js
@@ -1,12 +1,16 @@
 var AipOcrClient = require("baidu-aip-sdk").ocr;
 var fs = require('fs');
 var superagent = require('superagent');
-var images = require("images");
+var { APP_ID, API_KEY, SECRET_KEY } = require('../ocr.config');
+
+var fs = require('fs')
+  , gm = require('gm')
+
 
 // 设置APPID/AK/SK
-var APP_ID = "APP_ID";
-var API_KEY = "API_KEY";
-var SECRET_KEY = "SECRET_KEY";
+// var APP_ID = "APP_ID";
+// var API_KEY = "API_KEY";
+// var SECRET_KEY = "SECRET_KEY";
 
 // 新建一个对象，建议只保存一个对象调用服务接口
 var client = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
@@ -15,20 +19,41 @@ var client = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
 
   try {
     const res = await superagent.get('http://xz.jvtc.jx.cn/JVTC_XG/default3.html');
+    aaa = gm(res.body, 'd.gif')
+      // .limit('memory', '1024MB')
+      .toBuffer('jpg', function (err, buffer) {
+        // console.log(err);
+        if (err) return (err);
+        // console.log(buffer.toString('base64'));
 
-    var a = images(res.body).encode("jpg", { operation: 100 });
+        client.generalBasic(buffer.toString("base64")).then(function (result) {
+          const words_result = result['words_result']
+          console.log(result,words_result && words_result.length && words_result[0].words);
 
-    client.generalBasic(a.toString("base64")).then(function (result) {
-     const words_result = result['words_result']
-      console.log(words_result && words_result.length && words_result[0].words);
+        }).catch(function (err) {
+          // 如果发生网络错误
+          console.log(err);
+        });
 
-    }).catch(function (err) {
-      // 如果发生网络错误
-      console.log(err);
-    });
+      })
+
+    // console.log(aaa);
+
+
+    // var a = images(res.body).encode("jpg", { operation: 100 });
+
+    // client.generalBasic(a.toString("base64")).then(function (result) {
+    //  const words_result = result['words_result']
+    //   console.log(words_result && words_result.length && words_result[0].words);
+
+    // }).catch(function (err) {
+    //   // 如果发生网络错误
+    //   console.log(err);
+    // });
   } catch (err) {
     console.error(err);
   }
 
 })();
+
 
